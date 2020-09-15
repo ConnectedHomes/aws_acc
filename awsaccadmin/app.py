7c84ddbd79c049f820abce6ca9ebffa550666535
debswade <deborah.wade@hivehome.com>
2020-09-15 14:41:22 +0100

    Removed extra print statements

--- awsaccadmin/app.py
+++ awsaccadmin/app.py
@@ -1,15 +1,12 @@
-from urllib.parse import parse_qs
-from chalice import Chalice, AuthResponse
+from chalice import Chalice
 from chalicelib import db
 import boto3
 import os
 import json
-from ast import literal_eval
 
 app = Chalice(app_name="awsaccadmin")
 app.debug = True
 _DB = None
-_USER_DB = None
 
 def get_app_db():
     global _DB
@@ -17,12 +14,6 @@ def get_app_db():
         _DB = db.DynamoDBAWSAcc(
             boto3.resource("dynamodb").Table(os.environ["APP_TABLE_NAME"]) )
     return _DB
-
-def get_users_db():
-    global _USER_DB
-    if _USER_DB is None:
-        _USER_DB = boto3.resource("dynamodb").Table(os.environ["USERS_TABLE_NAME"])
-    return _USER_DB
 
 @app.route("/awsacc", methods=["GET"])
 def get_awsacc():
@@ -39,32 +30,15 @@ def get_account(accno):
 @app.route("/awsacc", methods=["POST"])
 def add_new_account():
     body = app.current_request.json_body
-    # accitem = {}
-    # accitem[ "AccountNumber" ] = body[ "AccountNumber" ]
-    # accitem[ 'AccountName' ] = body['AccountName']
-    # accitem[ 'Active' ] = body['Active']
-    # accitem[ 'Description' ] = body[ 'Description' ]
-    # accitem[ 'RealUsers ' ] = body[ 'RealUsers' ]
-    # accitem[ 'AccOwners' ] = body[ 'AccOwners' ]
-    # accitem[ 'OwnerTeam' ]= body[ 'OwnerTeam' ]
-    # accitem[ 'PreviousName' ]= body[ 'PreviousName' ]
-    # accitem[ 'SecOpsEmail' ]= body[ 'SecOpsEmail' ]
-    # accitem[ 'SecOpsSlackChannel' ]= body[ 'SecOpsSlackChannel' ]
-    # accitem[ 'TeamEmail'] = body[ 'TeamEmail']
     return get_app_db().add_item(body)
-
 
 @app.route("/awsacc/{accno}", methods=["DELETE"])
 def delete_account(accno):
     print(f"DELETE Account: {accno} ")
     return get_app_db().delete_item( AccountNumber=accno)
 
-
 @app.route("/awsacc/{accno}", methods=["PUT"])
 def update_account(accno):
     body = app.current_request.json_body
     print(type(body))
     get_app_db().update_item( accno, body)
-        # Description=body.get("Description"),
-        # Active=body.get("Active"),
-        # metadata=body.get("metadata")
--- awsaccadmin/chalicelib/db.py
+++ awsaccadmin/chalicelib/db.py
@@ -1,73 +1,23 @@
 from uuid import uuid4
-
 from boto3.dynamodb.conditions import Key
 
-DEFAULT_USERNAME = 'default'
-# DEFAULT_ACCOUNTNUMBER = '165954058622'
 DEFAULT_ACCOUNTNUMBER = '039759963043'
-DEFAULT_ACCOUNTNAME = 'AWS Lowes Scale'
 
 class AWSAccDB(object):
     def list_items(self):
         pass
 
-    def add_item(self, description, metadata=None):
+    def add_item(self, accitem):
         pass
-
-    def get_item(self, uid):
-        pass
-
-    def delete_item(self, uid):
-        pass
-
-    def update_item(self, uid, description=None, state=None,
-                    metadata=None):
-        pass
-
-
-class InMemoryAWSAccDB(AWSAccDB):
-    def __init__(self, state=None):
-        if state is None:
-            state = {}
-        self._state = state
-
-    def list_all_items(self):
-        all_items = []
-        for AccountNumber in self._state:
-            all_items.extend(self.list_items(AccountNumber))
-        return all_items
-
-    def list_items(self, AccountNumber=DEFAULT_ACCOUNTNAME):
-        return self._state.get(AccountNumber, {}).values()
-
-    def add_item(self, description=None, metadata=None,
-                 AccountNumber=DEFAULT_ACCOUNTNUMBER, AccountName=DEFAULT_ACCOUNTNAME):
-        if AccountNumber not in self._state:
-            self._state[username] = {}
-        self._state[AccountNumber][AccountNumber] = {
-            'AccountNumber': AccountNumber,
-            'Description': description,
-            'Active': 'N',
-            'metadata': metadata if metadata is not None else {},
-            'AccountName': AccountName
-        }
-        return accno
 
     def get_item(self, AccountNumber):
-        return self._state[AccountName][AccountNumber]
+        pass
 
     def delete_item(self, AccountNumber):
-        del self._state[AccountNumber]
+        pass
 
-    def update_item(self, description=None, metadata=None, Active=None,
-                 AccountNumber=DEFAULT_ACCOUNTNUMBER, AccountName=DEFAULT_ACCOUNTNAME):
-        item = self._state[AccountNumber][AccountName]
-        if description is not None:
-            item['Description'] = description
-        if Active is not None:
-            item['Active'] = state
-        if metadata is not None:
-            item['Metadata'] = metadata
+    def update_item(self, AccountNumber, jsonbody):
+        pass
 
 
 class DynamoDBAWSAcc(AWSAccDB):
@@ -79,40 +29,31 @@ class DynamoDBAWSAcc(AWSAccDB):
         return response['Items']
 
     def list_items(self, AccountNumber=DEFAULT_ACCOUNTNUMBER):
-        # print(f"{self._table}, {AccountNumber}")
         response = self._table.query(
             KeyConditionExpression=Key('AccountNumber').eq(AccountNumber)
         )
         return response['Items']
 
     def add_item(self, accitem ):
-        print(f"{self._table}, {accitem['AccountNumber']}")
-        self._table.put_item(Item=accitem)
-        return accitem['AccountNumber']
+        response = self._table.put_item(Item=accitem)
+        return response
 
     def get_item(self, AccountNumber):
         response = self._table.get_item(
-            Key={
-                'AccountNumber': AccountNumber
-            },
-        )
+            Key={'AccountNumber': AccountNumber} )
         if 'Item' in response:
             return response['Item']
         else:
             return f"Account {AccountNumber} not found."
 
     def delete_item(self, AccountNumber=None):
-        print(f"DELETE Account: {AccountNumber} {self._table}")
         response = self._table.delete_item(
-            Key={'AccountNumber': AccountNumber}
-        )
+            Key={'AccountNumber': AccountNumber} )
         return response
 
     def update_item(self, AccountNumber, jsonbody):
-        # We could also use update_item() with an UpdateExpression.
         item = self.get_item(AccountNumber)
-        print(jsonbody.keys())
         for key in jsonbody:
             item[key] = jsonbody[key]
-        self._table.put_item(Item=item)
-        return AccountNumber
+        response = self._table.put_item(Item=item)
+        return response
